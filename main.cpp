fbd8c1509b4e531891fc577dcee45015cadec07b
miya-ryu <83804046+miya-ryu@users.noreply.github.com>
2023-07-24 13:43:11 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 
 void main() {
-	int a, b, c, d;
+	int a, b, c, d, e;
 
 	a = 9 + 9;
 	b = 9 - 9;
 	c = 9 * 9;
 	d = 9 / 9;
+	e = 9 % 9;
 
 	std::cout << "‘«‚µŽZ" << a << std::endl;
 	std::cout << "ˆø‚«ŽZ" << b << std::endl;
 	std::cout << "Š|‚¯ŽZ" << c << std::endl;
 	std::cout << "Š„‚èŽZ" << d << std::endl;
+	std::cout << "—]‚è" << e << std::endl;
 }
