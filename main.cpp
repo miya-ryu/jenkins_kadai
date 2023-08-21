09a04e6282afed0d2a10b46daf1f1928ec58381e
miya-ryu <83804046+miya-ryu@users.noreply.github.com>
2023-08-21 10:27:04 +0900

    test

--- main.cpp
+++ main.cpp
@@ -7,11 +7,11 @@ void main() {
 	b = 9 - 9;
 	c = 9 * 9;
 	d = 9 / 9;
-	e = 9 % 9;
+	//e = 9 % 9;
 
 	std::cout << "足し算" << a << std::endl;
 	std::cout << "引き算" << b << std::endl;
 	std::cout << "掛け算" << c << std::endl;
 	std::cout << "割り算" << d << std::endl;
-	std::cout << "余り" << e << std::endl;
+	//std::cout << "余り" << e << std::endl;
 }
